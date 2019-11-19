cc777fe5e97e5693d9848e46181b8f25e16a79e6
leixiaowu <279198074@qq.com>
2019-11-19 13:52:22 +0800

    dbmall

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,84 @@
-import Link from 'umi/link'
 import styles from './index.css';
-import { Input } from 'antd';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 export default function() {
   return (
     <div className={styles.normal}>
-      <h1>index</h1>
-      <Link to="/user">user</Link>
-      <Input placeholder="Basic usage" />
+      <div className={styles.cont}>
+        <Form  className="login-form">
+          <h1>地标商城后台管理系统</h1>
+          <Input
+          className={styles.wbk}
+          placeholder="请输入用户名"
+          prefix={<Icon type="user" style={{ color:'rgba(0,0,0,.25)'}} />}
+          />
+          <br/>
+          <br/>
+          <Input.Password 
+            className={styles.wbk}
+            placeholder="请输入密码"
+            prefix={<Icon type="unlock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          />
+          <br/>
+          <br/>
+          <Button type="primary"href="/user" block>登录</Button>
+        </Form>
+      </div>
     </div>
   );
 }
+/* class NormalLoginForm extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  render() {
+    // const {getFieldDecorator} = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(<Checkbox>Remember me</Checkbox>)}
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(NormalLoginForm);
+ReactDOM.render(<WrappedNormalLoginForm />, mountNode)
+export default NormalLoginForm; */
